def12ae65fc5f2b4d668af8c85a17a792c734dd0
Jared Grove <jaredkgrove@gmail.com>
2019-06-26 16:07:14 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -9,4 +9,18 @@
 # value types according to the spec
 class CreateCostumes < ActiveRecord::Migration[5.1]
 
+  def change
+    create_table :costumes do |t|
+      t.string :name
+      t.float :price
+      t.string :size
+      t.string :image_url
+      t.timestamps
+    end
+  end
 end 
+
+# name
+# price
+# size
+# image url
