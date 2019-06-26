e5f0a757a8deeb73ebcc3d64859aacbb99965dc6
Jared Grove <jaredkgrove@gmail.com>
2019-06-26 16:10:26 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,17 @@
 # Create your haunted_houses migration here
+class CreateHauntedHouses < ActiveRecord::Migration[5.1]
+
+  def change
+    create_table :haunted_houses do |t|
+      t.string :name
+      t.string :location
+      t.integer :costume_inventory
+      t.integer :employees
+      t.boolean :in_business?
+      t.string :opening_time
+      t.string :closing_time
+    end
+
+
+  end
+end
