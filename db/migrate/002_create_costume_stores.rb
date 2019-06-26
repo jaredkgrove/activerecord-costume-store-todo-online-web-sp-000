4bb981006d59d93a1dd308a1143b37a47270aa62
Jared Grove <jaredkgrove@gmail.com>
2019-06-26 16:10:02 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,25 @@
 # Create your costume_stores migration here
+class CreateCostumeStores < ActiveRecord::Migration[5.1]
+
+  def change
+    create_table :costume_stores do |t|
+      t.string :name
+      t.string :location
+      t.integer :costume_inventory
+      t.integer :employees
+      t.boolean :in_business?
+      t.string :opening_time
+      t.string :closing_time
+    end
+
+    
+  end
+end
+
+# name
+# location
+# number of costumes, or "costume inventory"
+# number of employees
+# whether or not it's still in business
+# opening time
+# closing time
