49fcf63ae5c9618e9297be0fa70cfa50ec259f77
Jared Grove <jaredkgrove@gmail.com>
2019-06-26 16:23:42 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -8,8 +8,8 @@ class CreateHauntedHouses < ActiveRecord::Migration[5.1]
       t.string :theme
       t.float :price
       t.boolean :family_friendly
-      t.timedate :opening_date
-      t.timedate :closing_date
+      t.datetime :opening_date
+      t.datetime :closing_date
       t.string :description
     end
   end
